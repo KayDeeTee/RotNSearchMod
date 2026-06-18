2645432f91ed5359ddcc07cd20c813e032295032
Cap LaBuff <lucasbeef00@gmail.com>
2026-06-18 05:49:21 +0200

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 namespace SearchMod;
 
-[BepInPlugin("rotn.katie.utils.searchmod", "SearchMod", "1.0.1")]
+[BepInPlugin("rotn.katie.utils.searchmod", "SearchMod", "1.0.2")]
 public class SearchModPlugin : RiftPlugin
 {
     public static string searchString = "";
